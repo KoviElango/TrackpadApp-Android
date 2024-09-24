f28514fcfff6bbc4f282336c3227d07d800bdfed
Koovendhan Elango <kovendhanelango@gmail.com>
2024-09-24 01:05:01 -0400

    Planning additional features with feature buttons

--- app/src/main/java/com/example/trackpadapp/BaseScreen.kt
+++ app/src/main/java/com/example/trackpadapp/BaseScreen.kt
@@ -7,14 +7,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Circle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,6 +52,54 @@ fun TrackpadInterface(udpManager: UDPManager) {
                     .weight(1f)
                     .fillMaxWidth()
             ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(2.dp)
+                        .weight(0.12f),
+                    verticalArrangement = Arrangement.Center
+                ){
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    IconButton(
+                        onClick = { /* Handle button click */ },
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally) // Align the button horizontally to the left
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Circle,
+                            contentDescription = "Media Control",
+                            tint = Color(0xFF276972),
+                            modifier = Modifier.size(48.dp)
+                        )
+                    }
+                    IconButton(
+                        onClick = { /* Handle button click */ },
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally) // Align the button horizontally to the left
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Circle,
+                            contentDescription = "Media Control",
+                            tint = Color(0xFF276972),
+                            modifier = Modifier.size(48.dp)
+                        )
+                    }
+                    IconButton(
+                        onClick = { /* Handle button click */ },
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally) // Align the button horizontally to the left
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Circle,
+                            contentDescription = "Media Control",
+                            tint = Color(0xFF276972),
+                            modifier = Modifier.size(48.dp)
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.weight(3f))
+                }
                 Box(
                     modifier = Modifier
                         .weight(1f)
---
+++ app/src/main/java/com/example/trackpadapp/QuickButtons.kt
@@ -0,0 +1,9 @@
+package com.example.trackpadapp
+
+import UDPManager
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MediaControl(udpManager: UDPManager) {
+
+}
