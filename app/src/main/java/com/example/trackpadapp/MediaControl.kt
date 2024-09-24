6d56e6924fa4ab6b1f8893c39ff554e7967c4d1b
Koovendhan Elango <kovendhanelango@gmail.com>
2024-09-24 16:48:18 -0400

    Completed media control UI

--- app/src/main/java/com/example/trackpadapp/MediaControl.kt
+++ app/src/main/java/com/example/trackpadapp/MediaControl.kt
@@ -18,7 +18,6 @@ import androidx.compose.material.icons.outlined.VolumeDown
 import androidx.compose.material.icons.outlined.VolumeOff
 import androidx.compose.material.icons.outlined.VolumeUp
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
